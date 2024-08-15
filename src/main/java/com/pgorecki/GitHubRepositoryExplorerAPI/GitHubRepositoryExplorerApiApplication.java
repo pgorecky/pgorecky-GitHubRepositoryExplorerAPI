a1845cf51b3f0adbe54a07d4c6056ad5a8950650
Patryk <patryk.gorecky@gmail.com>
2024-08-15 19:17:28 +0200

    INIT_COMMIT

---
+++ src/main/java/com/pgorecki/GitHubRepositoryExplorerAPI/GitHubRepositoryExplorerApiApplication.java
@@ -0,0 +1,13 @@
+package com.pgorecki.GitHubRepositoryExplorerAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubRepositoryExplorerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitHubRepositoryExplorerApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pgorecki/GitHubRepositoryExplorerAPI/GitHubRepositoryExplorerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pgorecki.GitHubRepositoryExplorerAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubRepositoryExplorerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
